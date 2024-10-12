aa326af27d528b58d62c7ee9bfd1f222bcab86e3
NethskiEEE1 <kennethfabores03@gmail.com>
2024-10-12 21:21:04 +0800

    Update viewpdf.js

--- js/viewpdf.js
+++ js/viewpdf.js
@@ -2,5 +2,5 @@ document.getElementById('pdfLink').addEventListener('click', function(event) {
     event.preventDefault();
     // Here, you could open the PDF in a viewer or display a message
     // alert('Viewing PDF in a new tab instead of downloading.');
-    window.open('cv/cv.pdf', '_blank');
+    window.open('cv/CV.pdf', '_blank');
   });
